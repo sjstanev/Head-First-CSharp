4f0a15c91605e42b0802c14a764385db8fb95973
stambo jery <stambo_1@abv.bg>
2024-12-29 19:12:58 +0200

    finished chapter1

--- AnimalMatchingGame1/MainPage.xaml.cs
+++ AnimalMatchingGame1/MainPage.xaml.cs
@@ -34,11 +34,59 @@
                 button.Text = nextEmoji;
                 animalEmoji.RemoveAt(index);
             }
+            Dispatcher.StartTimer(TimeSpan.FromSeconds(.1), TimerTick);
         }
 
+        int tenthsOfSecondsElapsed = 0;
+        private bool TimerTick()
+        {
+            if (!this.IsLoaded) return false;
+            tenthsOfSecondsElapsed++;
+
+            TimeElapsed.Text = "Time elapsed: " +
+                (tenthsOfSecondsElapsed / 10F).ToString("0.0s");
+            if (PlayAgainButton.IsVisible)
+            {
+                tenthsOfSecondsElapsed = 0;
+                return false;
+            }
+            return true;
+        }
+
+        Button lastClicked;
+        bool findingMatch = false;
+        int matchesFound;
         private void Button_Clicked(object sender, EventArgs e)
         {
+            if (sender is Button buttonClicked) 
+            {
+                if (!string.IsNullOrWhiteSpace(buttonClicked.Text) && (findingMatch == false)) 
+                {
+                    buttonClicked.Background = Colors.Red;
+                    lastClicked = buttonClicked;
+                    findingMatch = true;
+                }
+                else
+                {
+                    if ((buttonClicked != lastClicked) && (buttonClicked.Text == lastClicked.Text) && (!String.IsNullOrWhiteSpace(buttonClicked.Text)))
+                    {
 
+                        matchesFound++;
+                        lastClicked.Text = "";
+                        buttonClicked.Text = "";
+                    }
+                    lastClicked.Background = Colors.LightBlue;
+                    buttonClicked.Background = Colors.LightBlue;
+                    findingMatch = false ;
+
+                }
+            }
+            if (matchesFound == 8) 
+            {
+                matchesFound = 0;
+                AnimalButtons.IsVisible = false;
+                PlayAgainButton.IsVisible = true;
+            } 
         }
     }
 
