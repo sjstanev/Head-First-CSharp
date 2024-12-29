85e66a29e24b1d004f86d772836ef0dbece66102
Stanimir Stanev <Stanimir Stanev@DELLXPS>
2024-12-29 15:47:45 +0200

    Add project files.

---
+++ AnimalMatchingGame1/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AnimalMatchingGame1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AnimalMatchingGame1/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿namespace AnimalMatchingGame1
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            AnimalButtons.IsVisible = false;
+        }
+
+        private void PlayAgainButton_Clicked(object sender, EventArgs e)
+        {
+            AnimalButtons.IsVisible = true;
+            PlayAgainButton.IsVisible = false;
+
+            List<string> animalEmoji = new List<string>()
+            {
+                "🐨", "🐨",
+                "🐶", "🐶",
+                "🐼", "🐼",
+                "🐘", "🐘",
+                "🐯", "🐯",
+                "🐧", "🐧",
+                "🦍", "🦍",
+                "🐇", "🐇",
+            };
+
+            foreach (var button in AnimalButtons.Children.OfType<Button>())
+            {
+                int index = Random.Shared.Next(animalEmoji.Count);
+                string nextEmoji = animalEmoji[index];
+                button.Text = nextEmoji;
+                animalEmoji.RemoveAt(index);
+            }
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
